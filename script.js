73bc85d12205a68edbf15e8168b4e66c5f3688ca
Mohammad hossein rasoulian <36469969+mhrasoulian@users.noreply.github.com>
2023-07-17 10:30:21 +0330

    Update script.js
    add some changes

--- script.js
+++ script.js
@@ -1,81 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>اسم فامیل</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-    }
-    h1 {
-      text-align: center;
-    }
-    label {
-      font-weight: bold;
-    }
-    #scoreboard {
-      margin-bottom: 20px;
-    }
-    #scoreboard table {
-      margin: 0 auto;
-      border-collapse: collapse;
-    }
-    #scoreboard th, #scoreboard td {
-      padding: 5px 10px;
-    }
-    #scoreboard th {
-      background-color: #f2f2f2;
-    }
-    #scoreboard td {
-      text-align: center;
-    }
-    #players {
-      text-align: center;
-    }
-    #players input {
-      margin-top: 10px;
-    }
-    #game, #results {
-      display: none;
-    }
-  </style>
-</head>
-<body>
-  <h1>بازی اسم فامیل</h1>
-  <div id="scoreboard">
-    <table>
-      <tr>
-        <th>نام بازیکن</th>
-        <th>امتیاز</th>
-      </tr>
-    </table>
-  </div>
-  <div id="players">
-    <label>تعداد بازیکن‌ها:</label>
-    <input type="number" id="numPlayers" min="2">
-    <button onclick="startGame()">شروع بازی</button>
-  </div>
-  <div id="game">
-    <label for="letter">حرف:</label>
-    <input type="text" id="letter">
-    <button onclick="submitAnswers()">ثبت</button>
-  </div>
-  <div id="results">
-    <h2>امتیازات نهایی</h2>
-    <table id="finalScores">
-      <tr>
-        <th>نام بازیکن</th>
-        <th>امتیاز</th>
-      </tr>
-    </table>
-  </div>
-  <script>
     var players = [];
     var currentPlayer = 0;
     var scores = {};
 
     function startGame() {
       var numPlayers = parseInt(document.getElementById("numPlayers").value);
-      if (numPlayers >= 2) {
+      if (numPlayers >= 2 && numPlayers <= 10) {
         for (var i = 0; i < numPlayers; i++) {
           var playerName = prompt("نام بازیکن " + (i + 1) + ":");
           if (playerName) {
@@ -89,7 +18,7 @@
         document.getElementById("players").style.display = "none";
         document.getElementById("game").style.display = "block";
       } else {
-        alert("حداقل باید دو بازیکن در بازی شرکت کنند.");
+        alert("تعداد بازیکن‌ها باید بین ۲ تا ۱۰ نفر باشد.");
       }
     }
 
@@ -165,4 +94,14 @@
       finalScoresTable.innerHTML = "";
       for (var i = 0; i < players.length; i++) {
         var playerRow = document.createElement("tr");
-        var playerName
+        var playerName = document.createElement("td");
+        playerName.innerText = players[i];
+        var playerScore = document.createElement("td");
+        playerScore.innerText = scores[players[i]];
+        playerRow.appendChild(playerName);
+        playerRow.appendChild(playerScore);
+        finalScoresTable.appendChild(playerRow);
+      }
+      document.getElementById("game").style.display = "none";
+      document.getElementById("results").style.display = "block";
+    }
